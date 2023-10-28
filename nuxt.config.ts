cf25c2788d8074bc468530b9e84ba9bf3c35af81
Nikita Fedotov <nikitka.kozarezov@mail.ru>
2023-10-28 15:51:19 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['kneekeetah-vue-ui-kit/dist/style.css', '~/style.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import { BaseButton } from 'kneekeetah-vue-ui-kit';
+    function goToStartPage() {
+        useRouter().push({ name: 'another' });
+    }
+</script>
+<template>
+    <div class="flex justify-center items-center flex-col gap-3 p-3 bg-gray-100 w-full h-screen">
+        <div class="w-[450px] text-center flex flex-col gap-5">
+            <h1 class="text-2xl  text-gray-700">Chess Online</h1>
+            <BaseButton outlined fill @click="goToStartPage">Start</BaseButton>
+            <div class="flex gap-2">
+                <nuxt-link to="https://github.com/FedotovN">Github</nuxt-link>
+                <nuxt-link to="https://t.me/nikitofedotov">Telegram</nuxt-link> 
+            </div>
+        </div>
+    </div>
+</template>
